45bc94a6fac0297f73f251837cf6545b96dfde9f
cedric <r0742311@student.thomasmore.be>
2019-10-16 11:42:55 +0200

    Opdracht 3: <=Eloquent models

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,37 +4,70 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-// Insert some users
-DB::table('users')->insert(
-    [
-        [
-            'name' => 'John Doe',
-            'email' => 'john.doe@example.com',
-            'admin' => true,
-            'password' => Hash::make('admin1234'),
-            'created_at' => now(),
-            'email_verified_at' => now()
-        ],
-        [
-            'name' => 'Jane Doe',
-            'email' => 'jane.doe@example.com',
-            'admin' => false,
-            'password' => Hash::make('user1234'),
-            'created_at' => now(),
-            'email_verified_at' => now()
-        ]
-    ]
-);
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->boolean('active')->default(true);
+            $table->boolean('admin')->default(false);
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
 
-// Add 40 dummy users inside a loop
-for ($i = 0; $i <= 40; $i++) {
-    DB::table('users')->insert(
-        [
-            'name' => "ITF User $i",
-            'email' => "itf_user_$i@mailinator.com",
-            'password' => Hash::make("itfuser$i"),
-            'created_at' => now(),
-            'email_verified_at' => now()
-        ]
-    );
+        // Insert some users
+        DB::table('users')->insert(
+            [
+                [
+                    'name' => 'Cedric Gijsels',
+                    'email' => 'Cedric.Gijsels@gmail.com',
+                    'admin' => true,
+                    'password' => Hash::make('admin1234'),
+                    'created_at' => now(),
+                    'email_verified_at' => now()
+                ],
+                [
+                    'name' => 'Jane Doe',
+                    'email' => 'jane.doe@example.com',
+                    'admin' => false,
+                    'password' => Hash::make('user1234'),
+                    'created_at' => now(),
+                    'email_verified_at' => now()
+                ]
+            ]
+        );
+
+        // Add 40 dummy users inside a loop
+        for ($i = 0; $i <= 40; $i++) {
+            DB::table('users')->insert(
+                [
+                    'name' => "ITF User $i",
+                    'email' => "itf_user_$i@mailinator.com",
+                    'password' => Hash::make("itfuser$i"),
+                    'created_at' => now(),
+                    'email_verified_at' => now()
+                ]
+            );
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
 }
